1586a7145ff08b4b6f06b637775cc5b8e733c878
Jayasank Weerasinghe <33048395+jayasanka-sack@users.noreply.github.com>
2026-03-19 16:19:36 +0530

    Update report paths

--- js/fetchReports.js
+++ js/fetchReports.js
@@ -4,13 +4,18 @@
  */
 export async function fetchReports() {
   const reports = [
-    "data/openmrs-core.json",
-    "data/openmrs-module-billing.json",
-    "data/openmrs-module-idgen.json",
+    "openmrs-core.json",
+    "openmrs-module-billing.json",
+    "openmrs-module-idgen.json",
+    "openmrs-module-patientflags.json",
+    "openmrs-module-referencedemodata.json",
+    "openmrs-module-webservices.rest.json",
+    "openmrs-module-attachments.json",
+    "openmrs-module-fhir2.json",
   ];
 
   const responses = await Promise.all(
-    reports.map((report) => fetch(report).then((r) => r.json())),
+    reports.map((report) => fetch(`data/${report}`).then((r) => r.json())),
   );
 
   return responses;
